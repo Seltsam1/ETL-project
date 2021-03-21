da79c9eb90b239db5f5d9bdc5cbbcf98c2fe56c1
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-03-20 19:45:08 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,21 +1,15 @@
-CREATE TABLE game_popularity (
-	id SERIAL UNIQUE,
-	game TEXT PRIMARY KEY,
+CREATE TABLE game_data (
+	id INT PRIMARY KEY,
+	game TEXT,
 	year INT,
 	month TEXT,
 	avg_players DECIMAL,
 	monthly_change DECIMAL,
 	max_players INT,
-	avg_max_percent TEXT
-);
-
-CREATE TABLE game_details (
-	id SERIAL UNIQUE,
-	game TEXT PRIMARY KEY,
+	avg_max_percent TEXT,
 	developer TEXT,
 	price_cents DECIMAL,
 	description TEXT
 );
 
-SELECT * FROM game_popularity;
-SELECT * FROM game_details;
+SELECT * FROM game_data;
